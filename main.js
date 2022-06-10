9604fca360046622b8169c631a7d1c94a362f4b9
StoneDigger <stonedigger@my-e-mail.net>
2022-06-10 17:01:28 +0200

    update

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 
 
-    this.mainloop = function() {
-        //console.clear();
-        //console.log(">>> start check of resources");
-        var game = unsafeWindow.game;
+this.mainloop = function() {
+    //console.clear();
+    //console.log(">>> start check of resources");
+    var game = unsafeWindow.game;
 
-        manageUpgrades(game);
-        manageJobs(game, CFG);
-        manageHousings(game, CFG);
-        manageStorages(game, CFG);
+    manageUpgrades(game);
+    manageJobs(game, CFG);
+    manageHousings(game, CFG);
+    manageStorages(game, CFG);
 
-        setTimeout(this.mainloop.bind(this), 1000);
-    };
+    setTimeout(this.mainloop.bind(this), 1000);
+};
 
-    this.mainloop();
+this.mainloop();
