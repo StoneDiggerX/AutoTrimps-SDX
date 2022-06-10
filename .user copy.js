2e9e081a2810796341a0da4ea37a937dc932b754
StoneDigger <stonedigger@my-e-mail.net>
2022-06-10 13:06:01 +0200

    update

---
+++ .user copy.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         AutoTrimps-StoneDigger
+// @version      1.0
+// @namespace    https://github.com/StoneDiggerX/AutoTrimps-StoneDigger
+// @updateURL    https://github.com/StoneDiggerX/AutoTrimps-StoneDigger/blob/main/.user.js
+// @description  Automate all the trimps!
+// @author       StoneDigger
+// @include      *trimps.github.io*
+// @connect      *github.com/StoneDiggerX/AutoTrimps-StoneDigger*
+// @connect      *trimps.github.io*
+// @connect      self
+// @grant        GM_xmlhttpRequest 
+// ==/UserScript==
+
+var script = document.createElement('script');
+script.id = 'AutoTrimps-Zek';
+//This can be edited to point to your own Github Repository URL.
+//script.src = 'https://github.com/StoneDiggerX/AutoTrimps-StoneDigger/blob/main/AutoTrimpsSD.js';
+script.src = 'https://raw.githubusercontent.com/StoneDiggerX/AutoTrimps-StoneDigger/main/AutoTrimpsSD.js?token=GHSAT0AAAAAABVPLSQ56Y3MSDQZS66TZAMQYVDD4MA'
+//script.setAttribute('crossorigin',"use-credentials");
+script.setAttribute('crossorigin',"anonymous");
+document.head.appendChild(script);
--- .user.js
+++ .user.js
@@ -1,22 +1,49 @@
 // ==UserScript==
 // @name         AutoTrimps-StoneDigger
-// @version      1.0
-// @namespace    https://github.com/StoneDiggerX/AutoTrimps-StoneDigger
-// @updateURL    https://github.com/StoneDiggerX/AutoTrimps-StoneDigger/blob/main/.user.js
-// @description  Automate all the trimps!
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Automate some trimps functions
 // @author       StoneDigger
-// @include      *trimps.github.io*
-// @connect      *github.com/StoneDiggerX/AutoTrimps-StoneDigger*
-// @connect      *trimps.github.io*
-// @connect      self
-// @grant        GM_xmlhttpRequest 
+// @match        *trimps.github.io*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        unsafeWindow
 // ==/UserScript==
+if(!unsafeWindow.settings){
+    unsafeWindow.settings = {
+        autoBuild:true
+    };
+}
+var autoTrimpSD = (function() {
 
-var script = document.createElement('script');
-script.id = 'AutoTrimps-Zek';
-//This can be edited to point to your own Github Repository URL.
-//script.src = 'https://github.com/StoneDiggerX/AutoTrimps-StoneDigger/blob/main/AutoTrimpsSD.js';
-script.src = 'https://raw.githubusercontent.com/StoneDiggerX/AutoTrimps-StoneDigger/main/AutoTrimpsSD.js?token=GHSAT0AAAAAABVPLSQ56Y3MSDQZS66TZAMQYVDD4MA'
-//script.setAttribute('crossorigin',"use-credentials");
-script.setAttribute('crossorigin',"anonymous");
-document.head.appendChild(script);
+    // Your code here...
+   var ATversion = 'SD v0.0.1',
+    atscript = document.getElementById('AutoTrimps-script'),
+    basepath = 'https://github.com/StoneDiggerX/AutoTrimps-StoneDigger', //Link to your own Github here if you forked!
+    modulepath = 'modules/';
+null !== atscript && (basepath = atscript.src.replace(/AutoTrimpsSD\.js$/, ''));
+
+function ATscriptLoad(a, b) {
+    null == b && console.log('Wrong Syntax. Script could not be loaded. Try ATscriptLoad(modulepath, \'example.js\'); ');
+    var c = document.createElement('script');
+    null == a && (a = ''), c.src = basepath + a + b + '.js', c.id = b + '_MODULE', document.head.appendChild(c)
+}
+
+function ATscriptUnload(a) {
+    var b = document.getElementById(a + "_MODULE");
+    b && (document.head.removeChild(b), console.log("Removing " + a + "_MODULE", "other"))
+}
+
+//var isSteam = false;
+
+function initializeAutoTrimps() {
+
+    ATmoduleList = ['import-export', 'query'];
+    for (var m in ATmoduleList) {
+        ATscriptLoad(modulepath, ATmoduleList[m]);
+    }
+    console.log('AutoTrimps-StoneDigger Loaded!');
+}
+
+initializeAutoTrimps();
+
+})();
