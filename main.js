23e651661893e92f0ac3a3602e192c31a732e9c9
StoneDigger <stonedigger@my-e-mail.net>
2022-06-10 17:36:03 +0200

    update

--- main.js
+++ main.js
@@ -1,19 +1,2 @@
 console.log("start load main module");
-
-this.mainloop = function() {
-    //console.clear();
-    //console.log(">>> start check of resources");
-    var game = unsafeWindow.game;
-
-    //manageUpgrades(game);
-    //manageJobs(game, CFG);
-    //manageHousings(game, CFG);
-    //manageStorages(game, CFG);
-
-    setTimeout(this.mainloop.bind(this), 1000);
-};
-
 console.log("execute main module");
-
-this.mainloop();
-
