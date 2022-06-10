53b269c6b6356e57537697dd7392c067db56cad3
StoneDigger <stonedigger@my-e-mail.net>
2022-06-10 12:01:21 +0200

    update

--- .user.js
+++ .user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
-// @name         AutoTrimps-Zek
-// @version      1.0-Zek
-// @namespace    https://Zorn192.github.io/AutoTrimps
-// @updateURL    https://Zorn192.github.io/AutoTrimps/.user.js
+// @name         AutoTrimps-StoneDigger
+// @version      1.0
+// @namespace    https://github.com/StoneDiggerX/AutoTrimps-StoneDigger
+// @updateURL    https://github.com/StoneDiggerX/AutoTrimps-StoneDigger/blob/main/.user.js
 // @description  Automate all the trimps!
-// @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0
+// @author       StoneDigger
 // @include      *trimps.github.io*
-// @include      *kongregate.com/games/GreenSatellite/trimps
-// @connect      *Zorn192.github.io/AutoTrimps*
+// @connect      *github.com/StoneDiggerX/AutoTrimps-StoneDigger*
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        GM_xmlhttpRequest 
@@ -16,7 +15,7 @@
 var script = document.createElement('script');
 script.id = 'AutoTrimps-Zek';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://Zorn192.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://github.com/StoneDiggerX/AutoTrimps-StoneDigger/blob/main/AutoTrimpsSD.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
